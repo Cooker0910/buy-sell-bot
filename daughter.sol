c1a5d1dec120cbeb94f3b61e784b3764e8c3483b
Caesar0910 <cooker98910@gmail.com>
2022-05-07 03:34:15 +0900

    sol

--- daughter.sol
+++ daughter.sol
@@ -1,14 +1,12 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.13;
+
 contract DaughterContract {
-    string public name;
-    uint public age;
-    constructor(
-        string memory _daughtersName,
-        uint _daughtersAge
-    )
-    public
-    {
-        name = _daughtersName;
-        age = _daughtersAge;
-    }
+  string public name;
+  uint public age;
+  public
+  {
+    name = 'daugher';
+    age = _daughtersAge;
+  }
 }
