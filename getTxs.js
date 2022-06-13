b68aa919e19c06237a588ce32f1377cfffb28f60
cooker0910 <cooker98910@gmail.com>
2022-06-13 15:07:27 -0700

    update

--- getTxs.js
+++ getTxs.js
@@ -1,22 +1,23 @@
-async function checkTransactionCount(startBlockNumber, endBlockNumber) {
-  console.log(
-    "Searching for non-zero transaction counts between blocks " +
-      startBlockNumber +
-      " and " +
-      endBlockNumber
-  );
+const Web3 = require("Web3");
+// const ENDPOINT = "https://mainnet.infura.io/v3/0e42c582d71b4ba5a8750f688fce07da";
+const ENDPOINT = "https://data-seed-prebsc-1-s1.binance.org:8545/";
 
-  for (var i = startBlockNumber; i <= endBlockNumber; i++) {
-    let block = await web3.eth.getBlock(i);
-    if (block != null) {
-      if (block.transactions != null && block.transactions.length != 0) {
-        console.log(
-          "\n\nBlock #" + i + " has " + block.transactions + " transactions"
-        );
-      }
+let zyme = '0xEded14271227967cD24970dbF43CBc580D21A8e7';
+async function getERC20Transfers() {
+  let web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/0e42c582d71b4ba5a8750f688fce07da"));
+  let block = await web3.eth.getBlock(14925000);
+  for (var transactionIndex in block.transactions) {
+    let transactionHash = block.transactions[transactionIndex];
+    let transaction = await web3.eth.getTransaction(transactionHash);
+    console.log(1)
+    if (transaction.input.substr(0,10) == "0x095ea7b3") {
+      let length = transaction.input;
+      let address = length.substr(34, 74).toLowerCase();
+      if(address == '')
+      console.log(transaction)
     }
   }
 }
 
-
-checkTransactionCount();
+// main();
+getERC20Transfers();
